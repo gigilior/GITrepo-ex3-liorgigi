b43d581a87580c2f61f313ddddd57a8c36732c63
gigilior <liorgigilior@gmail.com>
2025-11-17 17:53:05 +0200

    changed Liorgigi.java print

--- Liorgigi.java
+++ Liorgigi.java
@@ -1,7 +1,7 @@
 public class Liorgigi {
     public static void main(String[] args) {
-        System.out.println("Lior Gigi");
-        System.out.println("Lior Gigi");
-        System.out.println("Lior Gigi");
+        System.out.println("Lior Gigi test");
+        System.out.println("Lior Gigi test");
+        System.out.println("Lior Gigi test");
     }
 }
